4b2d0e2eef4748dfb350723370f91f9174a1b50d
codewithmike-jpg <mikeyxofficial1234@gmail.com>
2025-03-06 15:03:53 +0100

    fonts

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,40 +15,40 @@ function Skills() {
       </div>
       <div className="md:mx-20 mx-10 mt-15 font-lato justify-center">
         <div class="grid md:grid-cols-4 gap-4  grid-cols-2">
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">HTML</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">CSS</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">Tailwind</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">MUI</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">JavaScript</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">TypeScript</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">React js</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">Next js</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">Git</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">Github</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">ReactNative</h1>
           </div>
-          <div className="px-6 py-6 bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
+          <div className="px-6 py-6 w-full rounded-xl bg-[#061417] mb-4 border-t-2 border-r-2 border-transparent hover:border-t-textGreen hover:border-r-textGreen transition-all duration-300 ease-in-out hover:shadow-md hover:-translate-y-0.5">
             <h1 className="text-white text-xl text-center">.....</h1>
           </div>
         </div>
